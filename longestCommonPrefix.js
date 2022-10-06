73cbe6622e0a5df078d68451b440c52b0cdca85a
mi5ha6in <mi5ha6in@gmail.com>
2022-10-06 23:43:44 +0300

    adds longestCommonPrefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/longest-common-prefix/
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length == 0) {
+    return "";
+  }
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) != 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+      if (prefix.length === 0) {
+        return "";
+      }
+    }
+  }
+  return prefix;
+};
+
+
+console.log(longestCommonPrefix(["flower","flow","flight"])); // "fl"
+console.log(longestCommonPrefix(["dog","racecar","car"])); // ""
+git
