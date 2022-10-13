23b4597c830a80df20e5feb4f7e711e9ac1115e3
mi5ha6in <mi5ha6in@gmail.com>
2022-10-13 20:02:19 +0300

    adds mergeTwoSortedList

---
+++ mergeTwoSortedList.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+let list1 = new ListNode(1);
+list1.next = new ListNode(2);
+list1.next.next = new ListNode(4);
+
+let list2 = new ListNode(1);
+list2.next = new ListNode(3);
+list2.next.next = new ListNode(4);
+
+/**
+ *
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function mergeTwoLists(list1, list2) {
+  let dummy = new ListNode(-Infinity);
+  let temp = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      temp.next = list1;
+      list1 = list1.next;
+    } else {
+      temp.next = list2;
+      list2 = list2.next;
+    }
+    temp = temp.next;
+  }
+
+  temp.next = list1 ? list1 : list2;
+
+  return dummy.next;
+}
+
+console.log(mergeTwoLists(list1, list2));
